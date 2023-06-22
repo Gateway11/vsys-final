d8afbef43ef2b4ec61b47749e4b14d59ab9c5640
Gateway11 <daixiang789@icloud.com>
2023-06-23 00:16:51 +0800

    update

--- make-for-android.sh
+++ make-for-android.sh
@@ -1,18 +1,31 @@
+#!/usr/bin/env bash
+
 cd 3rd-party
 
-git clone git@github.com:flame/blis.git
-configure auto
+if [ ! -d blis ]; then
+    git clone git@github.com:flame/blis.git
+    configure auto
 
-wget https://www.fftw.org/fftw-3.3.10.tar.gz
-tar -zxvf fftw-3.3.10.tar.gz
+    mkdir armeabi-v7a/blis-new
+    find blis -name *.h -exec cp {} ./armeabi-v7a/blis-new \;
+    find ./armeabi-v7a/blis-new -name "test*" | xargs rm -rf
+fi
 
-cd fftw-3.3.10
-mkdir build
-cd build
-cmake .. \
-    -DCMAKE_TOOLCHAIN_FILE=../../../../toolbox/ndk-r21/build/cmake/android.toolchain.cmake \
-    -DANDROID_ABI=armeabi-v7a \
-    -DANDROID_PLATFORM=android-29
+if [ ! -d fftw-3.3.10 ]; then
+    wget https://www.fftw.org/fftw-3.3.10.tar.gz
+    tar -zxvf fftw-3.3.10.tar.gz
 
-cd ../../../jni
+    cd fftw-3.3.10
+    mkdir build
+    cd build
+    cmake .. \
+        -DCMAKE_TOOLCHAIN_FILE=../../../../toolbox/ndk-r21/build/cmake/android.toolchain.cmake \
+        -DANDROID_ABI=armeabi-v7a \
+        -DANDROID_PLATFORM=android-29
+
+    cd ../..
+fi
+
+cd ../jni
+
 ndk-build
