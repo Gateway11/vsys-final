a70314210a509f0b913bf10558387ef5ba0d9f6c
Gateway11 <daixiang789@icloud.com>
2023-06-23 22:10:51 +0800

    update

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,6 +1,6 @@
 APP_ABI                 := armeabi-v7a #arm64-v8a
-APP_STL                 := c++_static
 APP_CFLAGS              := -O3
 APP_CPPFLAGS            := -std=c++11 -frtti -Wno-register #-fpermissive
 APP_PLATFORM            := android-29
+APP_STL                 := c++_static
 NDK_TOOLCHAIN_VERSION   := 4.9
