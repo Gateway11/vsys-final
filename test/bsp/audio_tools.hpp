a715ace2ef06c6aa5726548b70dd73041b191214
Gateway11 <daixiang789@icloud.com>
2023-07-02 16:29:11 +0800

    update

---
+++ test/bsp/audio_tools.hpp
@@ -0,0 +1,114 @@
+//
+//  audio_tools.hpp
+//  audio_tools
+//
+//  Created by 薯条 on 2023/3/31.
+//
+
+#ifndef AUDIO_TOOLS_HPP
+#define AUDIO_TOOLS_HPP
+
+#if 1
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <vector>
+
+#define FILE_NAME_SIZE 128
+typedef struct output_desc {
+    int output;
+    char path[FILE_NAME_SIZE];
+    void *next;
+} output_desc_t;
+
+const char* path = "~/Desktop/audio_tools";
+static pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+static output_desc* outputs = NULL;
+void easy_write(char* tag, uint32_t session, uint32_t pid, uint32_t device, uint32_t sample_rate,
+           uint32_t num_channels, uint32_t bits, char* data, size_t size, bool close) {
+    pthread_mutex_lock(&mutex);
+    output_desc_t* current_output = NULL;
+    char str[FILE_NAME_SIZE];
+    memset(str, 0, FILE_NAME_SIZE);
+    
+    sprintf(str, "%s/%s.session%d.pid%d.%#x_%d_%d_%dbit.pcm",
+            path, tag, session, pid, device, sample_rate, num_channels, bits);
+    
+    current_output = outputs;
+    while(current_output != NULL){
+        if(!strcmp(current_output->path, str)) {
+            break;
+        }
+        current_output = (output_desc_t *)current_output->next;
+    }
+    if(current_output == NULL || access(str, F_OK) < 0) {
+        if(current_output == NULL) {
+            current_output = (output_desc *)malloc(sizeof(output_desc_t));
+            memcpy(current_output->path, str, FILE_NAME_SIZE);
+            if(outputs == NULL) {
+                outputs = current_output;
+            } else {
+                output_desc_t* last_output = outputs;
+                while(last_output->next != NULL){
+                    last_output = (output_desc_t *)last_output->next;
+                }
+                last_output->next = current_output;
+            }
+            current_output->next = NULL;
+        }
+        current_output->output = open(str, O_WRONLY|O_CREAT|O_APPEND, S_IWUSR|S_IRGRP|S_IROTH);
+    }
+    write(current_output->output, data, size);
+    pthread_mutex_unlock(&mutex);
+}
+#endif
+#if 0
+
+#include <map>
+#include <mutex>
+#include <fstream>
+
+#ifdef __cplusplus
+extern "C"{
+#endif
+ // code ......
+
+#ifdef __cplusplus
+}
+#endif
+
+static std::map<std::string, std::ofstream> outputs;
+static std::mutex m_write;
+
+const char* path = "/data/debuglogger/audio_dump";
+const char* path2 = "/Users/daixiang/Desktop/audio_tools";
+
+void easy_write(std::string tag, uint32_t session, uint32_t pid, uint32_t device, uint32_t sample_rate,
+           uint32_t num_channels, uint32_t bits, char* data, size_t size, bool close) {
+    
+    std::lock_guard<std::mutex> lg(m_write);
+    std::ofstream output_stream;
+    char str[128];
+    
+    sprintf(str, "%s/%s.session%d.pid%d.%#x_%d_%d_%dbit.pcm",
+            path2, tag.c_str(), session, pid, device, sample_rate, num_channels, bits);
+    
+    auto iter = outputs.find(str);
+    if(iter == outputs.end() || iter->second.bad()) {
+        output_stream = std::ofstream(str, std::ios::out | std::ios::binary);
+    } else {
+        output_stream = std::move(iter->second);
+    }
+    printf("%s \t\t     %llu      %p\n", str, output_stream.tellp() / size, data);
+    
+    output_stream.write((char *)data, size);
+    //output_stream.flush();
+    
+    outputs[str] = std::move(output_stream);
+}
+#endif
+
+#endif /* AUDIO_TOOLS_HPP */
--- test/bsp/main.cpp
+++ test/bsp/main.cpp
@@ -9,9 +9,10 @@
 #include <thread>
 #include <vector>
 
-#include <stdio.h>
-#include <unistd.h>
 #include <sys/socket.h>
+#include <stdio.h>
+//#include <unistd.h>
+
 //#include <linux/types.h>
 //#include <linux/in.h>
 //#include <netinet/in.h>
@@ -21,10 +22,8 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
-#define IPC_DATA_MAX_SIZE 1024
-
 #define DP_CR5_SAF 0
-#define AUDIO_MONITOR_EPT 125 //8023
+#define AUDIO_MONITOR_EPT 2333
 
 /* user space needs this */
 #if !defined(AF_RPMSG) && !defined(__KERNEL__)
@@ -55,13 +54,10 @@ struct sockaddr_rpmsg {
 
 int output = open("/data/test.txt", O_WRONLY|O_CREAT|O_APPEND, S_IWUSR|S_IRGRP|S_IROTH);
 int main(int argc, const char * argv[]) {
-    //hal client
     //https://www.demo2s.com/c/c-sock-socket-af-rpmsg-sock-seqpacket-0.html
     //SemiDrive_X9_音频应用指南_Rev1.1.pdf -43
 #if 1
-    int32_t remote = 0, ret;
-    struct timeval tv = { .tv_usec = 300/* ms */ * 1000 };
-
+    int32_t serverfd, ret;
     uint32_t* data[1024], len;
 
     struct sockaddr_rpmsg servaddr, dst_addr;
@@ -70,113 +66,38 @@ int main(int argc, const char * argv[]) {
     servaddr.vproc_id = DP_CR5_SAF;
     servaddr.addr = AUDIO_MONITOR_EPT;
 
-    ALOGD("--------------- %d", __LINE__);
-    if ((remote = socket(PF_RPMSG, SOCK_SEQPACKET, 0)) != -1) {
-        if (setsockopt(remote, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0)
+    if ((serverfd = socket(PF_RPMSG, SOCK_SEQPACKET, 0)) != -1) {
+        struct timeval tv = { .tv_usec = 300/* ms */ * 1000 };
+        if (setsockopt(serverfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0)
             ALOGE("Set rcv timeo failed");
-        if (setsockopt(remote, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv)) < 0)
+        if (setsockopt(serverfd, SOL_SOCKET, SO_SNDTIMEO, &tv, sizeof(tv)) < 0)
             ALOGE("Set snd timeo failed");
-        ret = connect(remote, (struct sockaddr *)&servaddr, sizeof(servaddr));
+        ret = connect(serverfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
         while (true) {
-            ret = write(remote, data, len);
-            ret = read(remote, &ret, sizeof(ret));  //block
+            ret = recvfrom(serverfd, data, sizeof(data), 0, NULL, 0);
+            ret = sendto(serverfd, &ret, sizeof(ret), 0, NULL, 0);
+            //ret = write(serverfd, data, len);
+            //ret = read(serverfd, &ret, sizeof(ret));  //block
         }
     } else {
-        ALOGE("ipcc init failed, errno: %d (%s)", remote, strerror(errno));
-    }
-#else
-    int32_t listenfd, clientfd, ret;
-    uint32_t* data[1024], len;
-
-    struct sockaddr_rpmsg recvaddr, cliaddr;
-    bzero(&recvaddr, sizeof(recvaddr));
-    recvaddr.family = AF_RPMSG;
-    recvaddr.vproc_id = DP_CR5_SAF;
-    recvaddr.addr = AUDIO_MONITOR_EPT;
-
-    socklen_t clilen = sizeof(cliaddr);
-
-    if ((listenfd = socket(PF_RPMSG, SOCK_SEQPACKET, 0)) != -1) {
-        if (bind(listenfd, (struct sockaddr *)&recvaddr, sizeof(recvaddr)) != -1);
-        if (listen(listenfd, 5) == 0);
-        if ((clientfd = accept(listenfd, (struct sockaddr *)&cliaddr, &clilen)) != -1) {
-            printf("received from %s at PORT %d\n", inet_ntop(AF_INET,
-                        &cliaddr.sin_addr, str, sizeof(str)), ntohs(cliaddr.sin_port));
-        }
-        while (true) {
-            //ret = recvfrom(listenfd, data, sizeof(data), 0, &cliaddr, &clilen);
-            ret = read(clientfd, data, sizeof(data));  //block
-            ret = write(clientfd, data, len);
-        }
-    } else {
-        ALOGE("ipcc init failed, errno: %d (%s)", listenfd, strerror(errno));
+        ALOGE("ipcc init failed, errno: %d (%s)", serverfd, strerror(errno));
     }
 #endif
-
-
-#if 0
-    // safety server tcp/udp
-    int32_t receive, client, ret;
-    struct sockaddr_in recvaddr, cliaddr;
-    memset(&recvaddr, 0, sizeof(recvaddr));
-    socklen_t cliaddr_len = sizeof(cliaddr);
-
-    char ip_addr_str[32], str[32];
-    snprintf(ip_addr_str, sizeof(ip_addr_str), "%d.%d.%d.%d", ULINK_NET_NS0,
-             ULINK_NET_NS1, ULINK_NET_NS2, ULINK_IP_OFFSET/* + DP_CR5_SAF*/);
-
-    //ip4_addr_t ip;
-    //inet_aton(ip_addr_str, &ip);
-    //inet_addr_from_ip4addr(&recvaddr.sin_addr, &ip);
-
-    recvaddr.sin_family = AF_INET; //PF_INET
-    recvaddr.sin_addr.s_addr = inet_addr(ip_addr_str);
-    recvaddr.sin_port = PP_HTONS/*htons*/(SSA_LWIP_SERVER_PORT /* 8023 */);
-    //recvaddr.sin_len = sizeof(recvaddr);
-
-    if ((receive = lwip_socket(AF_INET, SOCK_STREAM, 0)) >= 0) {
-    //if ((receive = lwip_socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP) >= 0) {
-        if (lwip_bind(receive, (struct sockaddr *)&recvaddr, sizeof(recvaddr)) == 0) {
-            //struct timeval tv = { .tv_sec = 300/* ms */ * 1000, .tv_usec = 0 };
-            //setsockopt(receive, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
-            lwip_listen(socket, 20);
-            while (true) {
-                client = lwip_accept(receive, (struct sockaddr *)&cliaddr, &cliaddr_len);
-                //ret = lwip_recvfrom(receive, disp->recv_data, RECV_DATA, 0, &cliaddr, &cliaddr_len);
-                //need conf ?
-                //cliaddr.sin_family = PF_INET;
-                //cliaddr.sin_port = htons(8888);
-                //cliaddr.sin_addr.s_addr = inet_addr("172.20.2.41");
-                //lwip_sendto(receive, (char *)buf, len, 0, (struct sockaddr *)&cliaddr, sizeof(cliaddr));
-                printf("received from %s at PORT %d\n", inet_ntop(AF_INET,
-                            &cliaddr.sin_addr, str, sizeof(str)), ntohs(cliaddr.sin_port));
-                // TODO: check client IP is SSB ?
-                while (true) {
-                    ret = lwip_recv(client, &recv_data, sizeof(recv_data), 0);
-                    lwip_send(client, &ret, sizeof(ret), 0);
-                }
-            }
-            lwip_close(client);
-            lwip_close(receive);
-        }
-    }
-#endif
-#if 0
+#if 1
     //linux server tcp/udp
-    struct sockaddr_in servaddr, cliaddr;
+    struct sockaddr_in servaddr, recvaddr, cliaddr;
     int32_t receive, client, ret;
     socklen_t cliaddr_len = sizeof(cliaddr);
 
     bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-    //servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
     servaddr.sin_port = htons(2333);
 
     uint32_t* buf[1024], str[64];
 
     if ((receive = socket(AF_INET, SOCK_STREAM, 0)) != -1) {
-    //if ((recv_socket = socket(AF_INET, SOCK_DGRAM, 0)) != -1) {
+    //if ((receive = socket(AF_INET, SOCK_DGRAM, 0)) != -1) {
         if (bind(receive, (struct sockaddr *)&servaddr, sizeof(struct sockaddr_in)) == 0) {
             listen(recv_socket, 20);
             while (true) {
