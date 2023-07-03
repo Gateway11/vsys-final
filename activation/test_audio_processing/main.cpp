6b84a2227478e3aa6107adadbb3b6ebdea646db6
Gateway11 <daixiang789@icloud.com>
2023-07-03 20:47:17 +0800

    update

--- activation/test_audio_processing/main.cpp
+++ activation/test_audio_processing/main.cpp
@@ -1,140 +1,125 @@
 //
 //  main.cpp
-//  test_audio_processing
+//  aec
 //
-//  Created by 薯条 on 2018/3/24.
-//  Copyright © 2018年 薯条. All rights reserved.
+//  Created by 薯条 on 2018/10/30.
+//  Copyright © 2018 薯条. All rights reserved.
 //
-
-#define LOG_TAG "audio_procissing"
-
-#define MIC_CHANNEL 2
-#define SPEAKER_CHANNEL 2
-
-#define FRAME_SIZE 160
-#define FRAME_SIZE_SPEEX 256
-
-#define SAMPLE_RATE 48000
-#define SPEEX_AEC_TAIL 1024
 
 #include <fstream>
-#include <thread>
-#include <list>
 #include <vector>
+#include <thread>
+#include <atomic>
+#include <functional>
 #include <semaphore.h>
+#include <speex/speex_echo.h>
+#include <speex/speex_preprocess.h>
+
 #include "debug.h"
-#include "speex_preprocess.h"
-#include "speex_echo.h"
 
-std::ifstream input_stream("/Users/daixiang/Desktop/vsys/data/sounds/lujnan_M_0020.48000.4.16bit.pcm", std::ios::in | std::ios::binary);
-std::ofstream output_stream("/Users/daixiang/Desktop/vsys/data/sounds/lujnan_M_0020.48000.2.16bit.pcm", std::ios::out | std::ios::binary);
+uint32_t num_mics = 2;
+uint32_t num_speakers = 2;
+uint32_t num_channels = num_mics + num_speakers;
 
-char buff[8192];
-float input[8192];
-float output[FRAME_SIZE_SPEEX];
+uint32_t sample_rate = 16000;
+uint32_t frame_size = sample_rate / 100;
 
 void test_audio_processing(){
-    SpeexPreprocessState** preprocess_states = new SpeexPreprocessState*[MIC_CHANNEL];
-    SpeexEchoState** echo_states = new SpeexEchoState*[SPEAKER_CHANNEL];
+    std::ifstream input_stream("../data/lujnan_M_0020.48000.4.16bit.pcm", std::ios::in | std::ios::binary);
+    std::ofstream output_stream("../data/tmp/lujnan_G_0020.16000.2.16bit.pcm", std::ios::out | std::ios::binary);
     
-    int sample_rate = SAMPLE_RATE;
-    for(uint32_t i = 0; i < MIC_CHANNEL; i++){
-        preprocess_states[i] = speex_preprocess_state_init(FRAME_SIZE_SPEEX, SAMPLE_RATE);
-        echo_states[i] = speex_echo_state_init_mc(FRAME_SIZE_SPEEX, SPEEX_AEC_TAIL, 1, SPEAKER_CHANNEL);
+    SpeexPreprocessState** dens(new SpeexPreprocessState*[num_mics]);
+    SpeexEchoState** echo_states(new SpeexEchoState*[num_speakers]);
+    for(uint32_t i = 0; i < num_mics; i++){
+        dens[i] = speex_preprocess_state_init(frame_size, sample_rate);
         
+        echo_states[i] = speex_echo_state_init_mc(frame_size, 1024, 1, num_speakers);
         speex_echo_ctl(echo_states[i], SPEEX_ECHO_SET_SAMPLING_RATE, &sample_rate);
-        speex_preprocess_ctl(preprocess_states[i], SPEEX_PREPROCESS_SET_ECHO_STATE, echo_states[i]);
+        speex_preprocess_ctl(dens[i], SPEEX_PREPROCESS_SET_ECHO_STATE, echo_states[i]);
     }
     
-    std::list<uint32_t> tasks;
-    
-    sem_t resume, pause;
-    sem_t *resume_ref, *pause_ref;
-#if defined(__APPLE__) || defined(__MACH__)
-    sem_unlink("sem_speex_resume");
-    sem_unlink("sem_speex_pause");
-    resume_ref = sem_open("sem_speex_resume", O_CREAT | O_EXCL, 0644, 0);
-    pause_ref = sem_open("sem_speex_pause", O_CREAT | O_EXCL, 0644, 0);
-#else
-    sem_init(&resume, 0, 0);
-    sem_init(&pause, 0, 0);
-    resume_ref = &resume;
-    pause_ref = &pause;
+    sem_t *proc_sem, *sync_sem;
+#if defined(__APPLE__) && defined(__MACH__)
+#define __STR(x) #x
+    sem_unlink(__STR(proc_sem));
+    sem_unlink(__STR(sync_sem));
+    proc_sem = sem_open(__STR(proc_sem), O_CREAT|O_EXCL, 0644, 0);
+    sync_sem = sem_open(__STR(sync_sem), O_CREAT|O_EXCL, 0644, 0);
+#elif
+    sem_t __proc_sem, __sync_sem;
+    sem_init(&__proc_sem, 0, 0);
+    sem_init(&__sync_sem, 0, 0);
+    proc_sem = &__proc_sem;
+    sync_sem = &__sync_sem;
 #endif
-    
-    bool thread_exit = false;
-    std::mutex task_mutex;
-    auto thread_loop  = [&]{
-        std::unique_lock<decltype(task_mutex)> locker(task_mutex, std::defer_lock);
-        while (true) {
-            sem_wait(resume_ref);
-            if(thread_exit) break;
-            
-            locker.lock();
-            uint32_t task_id = *tasks.begin();
-            tasks.pop_front();
-            VSYS_DEBUGI("-------------------------------");
-            locker.unlock();
-            
-            speex_echo_cancellation(echo_states[task_id],
-                                    input + task_id * FRAME_SIZE_SPEEX,         //mic
-                                    input + MIC_CHANNEL * FRAME_SIZE_SPEEX,     //speaker
-                                    output + task_id * FRAME_SIZE_SPEEX);
-            speex_preprocess_run(preprocess_states[task_id], output + task_id * FRAME_SIZE_SPEEX);
-            
-            sem_post(pause_ref);
-        }
-    };
     
     std::vector<std::thread> threads;
-    for (uint32_t i = 0; i < 2; i++) {
-        threads.emplace_back(thread_loop);
+    std::atomic_int tasks(0);
+    std::atomic_bool thread_exit(false);
+    
+    short *data(new short[frame_size * num_channels]);
+    spx_int16_t* input(new spx_int16_t[num_channels * frame_size]);
+    spx_int16_t* output(new spx_int16_t[num_mics * frame_size]);
+    
+    for (uint32_t i = 0; i < num_mics; i++) {
+        threads.emplace_back([&]{
+            while (true) {
+                sem_wait(proc_sem);
+                if(thread_exit.load()) break;
+                
+                uint32_t task = tasks.fetch_sub(1, std::memory_order_seq_cst) - 1;
+                
+                speex_echo_cancellation(echo_states[task], input + task * frame_size,
+                                        input + frame_size * num_mics, output + task * frame_size);
+                speex_preprocess_run(dens[task], output + task * frame_size);
+                sem_post(sync_sem);
+            }
+        });
     }
     
-    uint32_t num_channels = MIC_CHANNEL + SPEAKER_CHANNEL;
-    uint32_t total = num_channels * FRAME_SIZE_SPEEX;
-    
+    auto mfor_each = [](uint32_t first, uint32_t last, std::function<void()> function){
+        for (uint32_t i = first; i < last; i++) {function();}
+    };
     while (input_stream.good()) {
-        input_stream.read(buff, total * sizeof(short));
-        for (uint32_t i = 0; i < num_channels; i++) {
-            for (uint32_t j = 0; j < total; j++) {
-                input[i * FRAME_SIZE_SPEEX + j] = ((short *)buff)[j * num_channels + i];
+        input_stream.read((char *)data, frame_size * num_channels * sizeof(short));
+        for (uint32_t i = 0; i < num_mics; i++) {
+            for (uint32_t j = 0; j < frame_size; j++) {
+                input[i * frame_size + j] = data[i + j * num_channels];
             }
         }
-        for (uint32_t i = FRAME_SIZE_SPEEX; i < num_channels; i++) {
-            for (uint32_t j = 0; j < MIC_CHANNEL; j++) {
-                input[i * SPEAKER_CHANNEL +j] = input[j * FRAME_SIZE_SPEEX + i];
+        for (uint32_t i = num_mics, k = 0; i < num_channels; i++, k++) {
+            for (uint32_t j = 0; j < frame_size; j++) {
+                input[k + j * num_mics + num_mics * frame_size] = data[i + j * num_channels];
             }
         }
-        for (uint32_t i = 0; i < MIC_CHANNEL; i++) {
-            tasks.push_back(i);
-            sem_post(resume_ref);
+        
+        tasks.store(num_mics, std::memory_order_release);
+        mfor_each(0, num_mics, [&proc_sem]{sem_post(proc_sem);});
+        mfor_each(0, num_mics, [&sync_sem]{sem_wait(sync_sem);});
+        
+        for (uint32_t i = 0; i < num_mics; i++) {
+            for (uint32_t j = 0; j < frame_size; j++) {
+                data[j * num_mics + i] = output[i * frame_size + j];
+            }
         }
-        for (uint32_t i = 0; i < MIC_CHANNEL; i++) {
-            sem_wait(pause_ref);
-        }
-        output_stream.write((char *)output, FRAME_SIZE_SPEEX * sizeof(float));
+        output_stream.write((char *)data, num_mics * frame_size * sizeof(short));
     }
-    
     thread_exit = true;
-    for (uint32_t i = 0; i < MIC_CHANNEL; i++) {
-        sem_post(resume_ref);
-        threads[i].join();
-    }
-    for (uint32_t i = 0; i < MIC_CHANNEL; i++) {
-        speex_echo_state_destroy(echo_states[i]);
-        speex_preprocess_state_destroy(preprocess_states[i]);
-    }
-    
-#if defined(__APPLE__) || defined(__MACH__)
-    sem_close(resume_ref);
-    sem_close(pause_ref);
-    sem_unlink("sem_speex_resume");
-    sem_unlink("sem_speex_pause");
+    mfor_each(0, num_mics, [&proc_sem]{sem_post(proc_sem);});
+    std::for_each(threads.begin(), threads.end(), [](std::thread& thread){thread.join();});
+#if defined(__APPLE__) && defined(__MACH__)
+    sem_close(proc_sem);
+    sem_close(sync_sem);
+    sem_unlink(__STR(proc_sem));
+    sem_unlink(__STR(sync_sem));
 #else
-    sem_destroy(resume_ref);
-    sem_destroy(pause_ref);
+    sem_destroy(proc_sem);
+    sem_destroy(sync_sem);
 #endif
+    for (uint32_t i = 0; i < num_mics; i++) {
+        speex_echo_state_destroy(echo_states[i]);
+        speex_preprocess_state_destroy(dens[i]);
+    }
 }
 
 int main(int argc, const char * argv[]) {
