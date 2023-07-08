7c94bc8cf8e33137a21e8f55e1233499517e5fc7
Gateway11 <daixiang789@icloud.com>
2023-07-08 20:25:18 +0800

    update

--- jni/Android.mk
+++ jni/Android.mk
@@ -11,21 +11,16 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := fftw
 LOCAL_SRC_FILES := ../3rd-party/$(TARGET_ARCH_ABI)/libs/libfftw3f.a
 #include $(PREBUILT_STATIC_LIBRARY)
-include Android-fftw3.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := blis
 LOCAL_SRC_FILES := ../3rd-party/$(TARGET_ARCH_ABI)/libs/libblis.a
 #include $(PREBUILT_STATIC_LIBRARY)
-include Android-blis.mk
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := r2ssp
 LOCAL_SRC_FILES := ../3rd-party/$(TARGET_ARCH_ABI)/libs/libr2ssp.so
 #include $(PREBUILT_SHARED_LIBRARY)
-include Android-r2ssp.mk 
-
-include Android-ztvad.mk Android-r2vt4.mk
 
 #include $(call all-subdir-makefiles)
-#include $(call all-named-files-under, Android-*, .)
+include $(call all-named-files-under,Android-*, .)
