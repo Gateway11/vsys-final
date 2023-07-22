1db257189d9fa01584c67cf4af7f2fcc15ad7de6
Gateway11 <daixiang789@icloud.com>
2023-07-22 13:23:32 +0800

    update

--- run.sh
+++ run.sh
@@ -3,9 +3,8 @@
 cd 3rd-party
 
 if [ ! -d blis/.git ]; then
-    git submodule update --init blis
+    git submodule update --init blis || exit; cd blis
 
-    cd blis
     export NDK_BUNDLE=../../../*/*
     export TOOLCHAIN=$NDK_BUNDLE/toolchains/llvm/prebuilt/$(uname | tr A-Z a-z)-x86_64/bin
     
