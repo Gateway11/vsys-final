b09b8a391e33baab307eda4be47ce2254f09a56f
Gateway11 <daixiang789@icloud.com>
2023-08-04 23:10:23 +0800

    update

--- test/swp64.cpp
+++ test/swp64.cpp
@@ -32,7 +32,7 @@ void my_printf(uint64_t reg, uint8_t reg_val, uint64_t val, uint8_t val_len) {
 	*((uint64_t *)buf) = __SWP64_MSB(reg, reg_val);
 	*((uint64_t *)(buf + reg_val)) = __SWP64_MSB(val, val_len);
 	
-	printf("Hello, World! %#llx, %#x, %#x, %#x, %#x, %#x, %#x, %#x, %#x, %#x, %#x, %#x, %#x, %#x, %#x, %#x, %#x\n", 
+	printf("Hello, World! %#llx, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x\n", 
 		   *(uint64_t *)buf, buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7], buf[8], buf[9], buf[10], buf[11], buf[12], buf[13], buf[14], buf[15]);
 }
 
@@ -44,7 +44,7 @@ int main()
     uint64_t number = ((uint64_t)reg << (vallen << 3)) | val;
     number = __SWP64_MSB(number, reglen + vallen);
 
-    printf("Hello, World! %#llx, %#x, %#x, %#x, %#x, %#x, %#x, %#x, %#x\n", number,
+    printf("Hello, World! %#llx, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x, %#.2x\n", number,
 	        ((uint8_t *)&number)[0], ((uint8_t *)&number)[1],
 	        ((uint8_t *)&number)[2], ((uint8_t *)&number)[3],
 	        ((uint8_t *)&number)[4], ((uint8_t *)&number)[5],
