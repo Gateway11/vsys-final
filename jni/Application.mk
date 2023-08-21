71c6a7ecee3246eda0b0c8d00a9cf0ea02047799
Gateway11 <daixiang789@icloud.com>
2023-08-21 17:23:38 +0800

    update

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,7 +1,6 @@
 APP_ABI                 := arm64-v8a #armeabi-v7a
 APP_CFLAGS              := -O3
 APP_CPPFLAGS            := -std=c++11 -frtti -Wno-register #-fpermissive
-#APP_LDFLAGS             := "-Wl,--start-group" -llog "-Wl,--end-group"
 APP_LDFLAGS             := -llog
 APP_PLATFORM            := android-32
 APP_STL                 := c++_static
