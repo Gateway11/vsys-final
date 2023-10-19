5934632cef4a9e3878ebd9ac4071b6334222f10c
daixiang <daixiang5@lenovo.com>
2023-10-19 10:24:52 +0800

    update

--- legacy/rokid-openvoice-process-android-pro/frameworks/native/services/openvoice_process/test_new/Android.mk
+++ legacy/rokid-openvoice-process-android-pro/frameworks/native/services/openvoice_process/test_new/Android.mk
@@ -13,3 +13,8 @@ LOCAL_SHARED_LIBRARIES := \
         android.hardware.automotive.audiocontrol@2.0
 
 include $(BUILD_EXECUTABLE)
+
+#out/soong/host/linux-x86/bin/hidl-gen -L hash -r android.hardware:hardware/interfaces android.hardware.audio@2.0
+#out/soong/host/linux-x86/bin/hidl-gen -L hash -r android.hardware:hardware/interfaces android.hardware.automotive.audiocontrol@2.0
+
+#development/vndk/tools/header-checker/utils/create_reference_dumps.py  -l android.hardware.audio@2.0 -product xxxx
--- legacy/rokid-openvoice-process-android-pro/frameworks/native/services/openvoice_process/test_new/test.cpp
+++ legacy/rokid-openvoice-process-android-pro/frameworks/native/services/openvoice_process/test_new/test.cpp
@@ -18,20 +18,11 @@ int32_t main(int argc, char** argv) {
         AudioParamConfig config;
         config.command = Audio_Command_Type::GEQ;
         config.equalizer.type = Audio_Geq_Type::EFFECT_TYPE;
-        config.equalizer.eq_effect_type[0] = 'P';
-        config.equalizer.eq_effect_type[1] = 'e';
-        config.equalizer.eq_effect_type[2] = 'r';
-        config.equalizer.eq_effect_type[3] = 'c';
-        config.equalizer.eq_effect_type[4] = 'u';
-        config.equalizer.eq_effect_type[5] = 's';
-        config.equalizer.eq_effect_type[6] = 's';
-        config.equalizer.eq_effect_type[7] = 'i';
-        config.equalizer.eq_effect_type[8] = 'o';
-        config.equalizer.eq_effect_type[9] = 'n';
+        config.equalizer.eq_effect_type = hardware::hidl_array<uint8_t, 16>({'P', 'e', 'r', 'c', 'u', 's', 's', 'i', 'o', 'n'});
         config.equalizer.frequency_index = 3;
-        config.values = 'A';
-        config.extra_data[0] = 'B';
-        config.extra_data[1] = 'C';
+        config.values = 2;
+        config.extra_data[0] = 1;
+        config.extra_data[1] = 3;
         config.extra_datalen = 2;
 
         printf("%s\n", toString(config).c_str());
