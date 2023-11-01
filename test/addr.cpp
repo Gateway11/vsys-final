31c504d95c99d154f0ae3f1cd82b3ddff63ba427
Gateway11 <daixiang789@icloud.com>
2023-11-01 21:58:44 +0800

    update

--- test/addr.cpp
+++ test/addr.cpp
@@ -64,30 +64,30 @@ int main()
     }
 #endif
 #if 0
-    android.hardware.automotive.audiocontrol.V2_0.MutingInfo[] mutingInfos =
+    android.hardware.automotive.audiocontrol.V2_0.MutingInfo[] mutingInfoToHal =
         new android.hardware.automotive.audiocontrol.V2_0.MutingInfo[1];
-    mutingInfos[0] = new android.hardware.automotive.audiocontrol.V2_0.MutingInfo();
+    mutingInfoToHal[0] = new android.hardware.automotive.audiocontrol.V2_0.MutingInfo();
 
-    int muteAddrNum = mutingInfos[0].deviceAddressesToMute.length;
-    if (carZonesMutingInfo.get(0).deviceAddressesToMute.length < mutingInfos[0].deviceAddressesToMute.length) {
+    int muteAddrNum = mutingInfoToHal[0].deviceAddressesToMute.length;
+    if (carZonesMutingInfo.get(0).deviceAddressesToMute.length < mutingInfoToHal[0].deviceAddressesToMute.length) {
         muteAddrNum = carZonesMutingInfo.get(0).deviceAddressesToMute.length;
     }
     for (int i = 0; i < muteAddrNum; i++) {
-        mutingInfos[0].deviceAddressesToMute[i] = carZonesMutingInfo.get(0).deviceAddressesToMute[i];
+        mutingInfoToHal[0].deviceAddressesToMute[i] = carZonesMutingInfo.get(0).deviceAddressesToMute[i];
     }
-    for (int i = muteAddrNum; i < mutingInfos[0].deviceAddressesToMute.length; i++) {
-        mutingInfos[0].deviceAddressesToMute[i] = "";
+    for (int i = muteAddrNum; i < mutingInfoToHal[0].deviceAddressesToMute.length; i++) {
+        mutingInfoToHal[0].deviceAddressesToMute[i] = "";
     }
 
-    int unMuteAddrNum = mutingInfos[0].deviceAddressesToUnmute.length;
-    if (carZonesMutingInfo.get(0).deviceAddressesToUnmute.length < mutingInfos[0].deviceAddressesToUnmute.length) {
+    int unMuteAddrNum = mutingInfoToHal[0].deviceAddressesToUnmute.length;
+    if (carZonesMutingInfo.get(0).deviceAddressesToUnmute.length < mutingInfoToHal[0].deviceAddressesToUnmute.length) {
         unMuteAddrNum = carZonesMutingInfo.get(0).deviceAddressesToUnmute.length;
     }
     for (int i = 0; i < unMuteAddrNum; i++) {
-        mutingInfos[0].deviceAddressesToUnmute[i] = carZonesMutingInfo.get(0).deviceAddressesToUnmute[i];
+        mutingInfoToHal[0].deviceAddressesToUnmute[i] = carZonesMutingInfo.get(0).deviceAddressesToUnmute[i];
     }
-    for (int i = unMuteAddrNum; i < mutingInfos[0].deviceAddressesToUnmute.length; i++) {
-        mutingInfos[0].deviceAddressesToUnmute[i] = "";
+    for (int i = unMuteAddrNum; i < mutingInfoToHal[0].deviceAddressesToUnmute.length; i++) {
+        mutingInfoToHal[0].deviceAddressesToUnmute[i] = "";
     }
 #endif
     //char* addr = addr_map[duckingInfo.usagesHoldingFocus[i].c_str()].c_str();
