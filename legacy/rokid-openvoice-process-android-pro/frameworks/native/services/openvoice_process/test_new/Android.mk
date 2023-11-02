fa739e36f76803759e8f41829382ec4dc546cd52
daixiang <daixiang5@lenovo.com>
2023-11-02 17:57:42 +0800

    update

--- legacy/rokid-openvoice-process-android-pro/frameworks/native/services/openvoice_process/test_new/Android.mk
+++ legacy/rokid-openvoice-process-android-pro/frameworks/native/services/openvoice_process/test_new/Android.mk
@@ -17,5 +17,6 @@ include $(BUILD_EXECUTABLE)
 #out/soong/host/linux-x86/bin/hidl-gen -L hash -r android.hardware:hardware/interfaces android.hardware.audio@2.0
 #out/soong/host/linux-x86/bin/hidl-gen -L hash -r android.hardware:hardware/interfaces android.hardware.automotive.audiocontrol@2.0
 #m android.hardware.automotive.audiocontrol-update-api
+#m android.hardware.automotive.audiocontrol-freeze-api
 
 #development/vndk/tools/header-checker/utils/create_reference_dumps.py -l android.hardware.audio@2.0 -product xxxx
