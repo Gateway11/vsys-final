97e029d9809792b4983b4867a6cfc6db45c76b65
daixiang <daixiang5@lenovo.com>
2023-11-16 16:55:13 +0800

    update

--- test/saf775d.cpp
+++ test/saf775d.cpp
@@ -6,19 +6,21 @@ int32_t main(int argc, char** argv)
 {   
 #define PI 3.1415926
 #define FREQUENCY 1060
+#if 0
     printf("%#X\n", (int32_t)(cos(PI * 2 * FREQUENCY / 48000) * 0x800000));
+    printf("%#X\n", 0x1000 - (int32_t)(pow(10, 0 - 12 / 20.0) * 2048));
     
     for (int32_t i = 1000; i < 20000; i++) {
         if (((int32_t)(cos(PI * 2 * i / 48000) * 0x800000)) == 0x7EC512) {
             printf("---------------------------- %d\n", i);
         }
     }
-    //printf("%#X\n", 0x1000 - (int32_t)(pow(10, 0 - 12 / 20.0) * 2048));
+#endif
     int32_t n = atoi(argv[1]);
     if (n >= 0) {
-        printf("%#X\n", (int32_t)(pow(10, n / 20.0) * 2048));
+        printf("cos(2πf/fs) * 0x800000  %#X\n", (int32_t)(cos(PI * 2 * n / 48000) * 0x800000));
     } else {
-        printf("%#X\n", 0x1000 - (int32_t)(pow(10, 0 - abs(n) / 20.0) * 2048));
+        printf("10^(vol / 20)           %#X\n", 0x1000 - (int32_t)(pow(10, 0 - abs(n) / 20.0) * 2048));
     }
 
     return 0;
