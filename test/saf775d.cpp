d96a5af60bba81c6018f840bc2684a87c475d660
daixiang <daixiang5@lenovo.com>
2023-11-16 16:46:07 +0800

    update

--- test/saf775d.cpp
+++ test/saf775d.cpp
@@ -2,7 +2,7 @@
 #include <math.h>
 #include <stdint.h>
 
-int main()
+int32_t main(int argc, char** argv)
 {   
 #define PI 3.1415926
 #define FREQUENCY 1060
@@ -13,6 +13,13 @@ int main()
             printf("---------------------------- %d\n", i);
         }
     }
-    printf("%#X\n", 0x1000 - (int32_t)(pow(10, 0 - 12 / 20.0) * 2048));
+    //printf("%#X\n", 0x1000 - (int32_t)(pow(10, 0 - 12 / 20.0) * 2048));
+    int32_t n = atoi(argv[1]);
+    if (n >= 0) {
+        printf("%#X\n", (int32_t)(pow(10, n / 20.0) * 2048));
+    } else {
+        printf("%#X\n", 0x1000 - (int32_t)(pow(10, 0 - abs(n) / 20.0) * 2048));
+    }
+
     return 0;
 }
