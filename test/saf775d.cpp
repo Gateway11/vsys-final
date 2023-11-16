cdff0e7ace0e76a95fd8a884392b0a0d9bc813e9
daixiang <daixiang5@lenovo.com>
2023-11-16 18:04:05 +0800

    update

--- test/saf775d.cpp
+++ test/saf775d.cpp
@@ -17,12 +17,29 @@ int32_t main(int argc, char** argv)
     }
     printf("%d\n",(int32_t)(log10((0x1000 - 0xF34) / 2048.0) * 20));
 #endif
+#if 1
+    for (int32_t i = 0; i < 62; i++) {
+        switch(0x1000 - (int32_t)(pow(10, 0 - abs(i) / 20.0) * 2048)) {
+        case 0xF34:
+        case 0xF8D:
+        case 0xFC0:
+        case 0xFDC:
+        case 0xFEC:
+        case 0xFF5:
+        case 0xFFA:
+        case 0xFFC:
+        case 0xFFE:
+        case 0xFFF:
+            printf("---------------------------- %d %#X\n", i,  0x1000 - (int32_t)(pow(10, 0 - abs(i) / 20.0) * 2048));
+        }
+    }
+#else
     int32_t n = atoi(argv[1]);
     if (n >= 0) {
         printf("cos(2πf/fs) * 0x800000  %#X\n", (int32_t)(cos(PI * 2 * n / 48000) * 0x800000));
     } else {
         printf("10^(vol / 20)           %#X\n", 0x1000 - (int32_t)(pow(10, 0 - abs(n) / 20.0) * 2048));
     }
-
+#endif
     return 0;
 }
