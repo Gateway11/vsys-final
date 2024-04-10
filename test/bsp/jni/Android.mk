42dbf62995f8b172c273c43da0fb7f5a9f3eadc7
Gateway11 <daixiang789@icloud.com>
2024-04-10 21:37:19 +0800

    update

--- test/bsp/jni/Android.mk
+++ test/bsp/jni/Android.mk
@@ -4,3 +4,8 @@ include $(CLEAR_VARS)
 LOCAL_MODULE    := ipc_test
 LOCAL_SRC_FILES := ../main.cpp
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE    := spidev_client
+LOCAL_SRC_FILES := ../spi/spidev_client.c
+include $(BUILD_EXECUTABLE)
---
+++ test/bsp/spi/spidev_api.h
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#ifdef __SPIDEV_API_H__
+#define __SPIDEV_API_H__
+
+#if 0
+// Open the SPI device
+// Parameters:
+// - device: SPI device identifier or name
+// - mode: Mode of SPI communication (e.g., read, write, read/write)
+// Returns:
+// - File descriptor of the opened SPI device
+int spi_open(const char *device, int mode);
+
+// Read data from SPI device
+// Parameters:
+// - fd: File descriptor of the SPI device
+// - buffer: Buffer to store the read data
+// - len: Number of bytes to read
+// Returns:
+// - Number of bytes successfully read, or -1 on error
+ssize_t spi_read(int fd, void *buffer, size_t len);
+
+// Write data to SPI device
+// Parameters:
+// - fd: File descriptor of the SPI device
+// - buffer: Data buffer to write
+// - len: Number of bytes to write
+// Returns:
+// - Number of bytes successfully written, or -1 on error
+ssize_t spi_write(int fd, const void *buffer, size_t len);
+
+// Transfer data to and from SPI device simultaneously
+// Parameters:
+// - fd: File descriptor of the SPI device
+// - tx_buffer: Data buffer to write
+// - rx_buffer: Buffer to store the read data
+// - len: Number of bytes to transfer
+// Returns:
+// - Number of bytes successfully transferred, or -1 on error
+ssize_t spi_transfer(int fd, const void *tx_buffer, void *rx_buffer, size_t len);
+
+// Close the SPI device
+// Parameters:
+// - fd: File descriptor of the SPI device
+// Returns:
+// - 0 on success, -1 on error
+int spi_close(int fd);
+
+#else
+
+// Open the SPI device
+// Returns:
+// - 0 on success, -1 on failure
+int spi_open();
+
+// Read data from SPI device
+// Parameters:
+// - buffer: Buffer to store the read data
+// - len: Number of bytes to read
+// Returns:
+// - Number of bytes successfully read, or -1 on error
+ssize_t spi_read(void *buffer, size_t len);
+
+// Write data to SPI device
+// Parameters:
+// - buffer: Data buffer to write
+// - len: Number of bytes to write
+// Returns:
+// - Number of bytes successfully written, or -1 on error
+ssize_t spi_write(const void *buffer, size_t len);
+
+// Transfer data to and from SPI device simultaneously
+// Parameters:
+// - tx_buffer: Data buffer to write
+// - rx_buffer: Buffer to store the read data
+// - len: Number of bytes to transfer
+// Returns:
+// - Number of bytes successfully transferred, or -1 on error
+ssize_t spi_transfer(const void *tx_buffer, void *rx_buffer, size_t len);
+
+// Close the SPI device
+// Returns:
+// - 0 on success, -1 on failure
+int spi_close();
+#endif
+
+#endif //__SPIDEV_API_H__
