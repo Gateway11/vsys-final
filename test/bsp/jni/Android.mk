f7750e95df753952624dc5f0bcad1863ef5e1b26
Gateway11 <daixiang789@icloud.com>
2024-04-22 21:29:39 +0800

    update

--- test/bsp/jni/Android.mk
+++ test/bsp/jni/Android.mk
@@ -7,11 +7,11 @@ include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := spidev_client
-LOCAL_SRC_FILES := ../spi/spidev_client.c
+LOCAL_SRC_FILES := ../spi/spidev_client.c ../spi/main.cpp
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := spidev_client-loop
 LOCAL_SRC_FILES := ../spi/spidev_client.cpp
 LOCAL_CPPFLAGS    := -Wno-writable-strings -Wno-reorder-init-list
-include $(BUILD_EXECUTABLE)
+#include $(BUILD_EXECUTABLE)
