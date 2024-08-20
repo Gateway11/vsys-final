9cf5f56bcd429b332746deccbe91bb4789f45cef
Gateway11 <daixiang789@icloud.com>
2024-08-20 22:16:49 +0800

    update

--- test/call_once.cpp
+++ test/call_once.cpp
@@ -5,8 +5,8 @@ int main()
 {
     std::cout << "Hello World" << std::endl;
     static std::once_flag flag;
+    int32_t timer = 5, timer2 = 5;
     std::call_once(flag, [&]{ std::thread thread([&]{
-                int32_t timer = 5;
                 while (--timer) {
                     //AHAL_DBG("################## timer = %d", timer);
                     printf("---------------------\n");
@@ -16,7 +16,6 @@ int main()
                 //AudioExtn::audio_extn_set_parameters(adev_, parms);
                 }); thread.detach(); }); 
 
-    int32_t timer2 = 5;
     while (--timer2) {
         printf("--ddddd-------------------\n");
         std::this_thread::sleep_for(std::chrono::seconds(1));
