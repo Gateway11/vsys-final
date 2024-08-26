daf79f532117689e3763079dbe1fa3bd22c0d7d1
daixiang <daixiang5@lenovo.com>
2024-08-26 10:52:19 +0800

    update

--- test/bsp/i2s/DPin_test.bat
+++ test/bsp/i2s/DPin_test.bat
@@ -5,8 +5,7 @@ adb disconnect 192.168.60.252
 adb wait-for-device
 adb shell touch /data/nfc
 if "%errorlevel%"=="1" (
-    adb -s a31089dc root
-    adb -s a31089dc remount
+    adb -s a31089dc root & adb -s a31089dc remount
 )
 
 ping -n 1 192.168.60.%1
@@ -29,14 +28,12 @@ if "%2"=="1" (
 
 set NOW_TIME=%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~0,2%-%time:~3,2%
 set NOW_TIME=%NOW_TIME: =0%
-MD %NOW_TIME%
-CD %NOW_TIME%
+MD %NOW_TIME% & CD %NOW_TIME%
 adb -s 192.168.60.%1 shell logcat -c
 adb -s 192.168.60.%1 shell logcat -c
 start "" cmd /c "adb -s 192.168.60.%1 shell logcat | tee log"
 
 if "%1"=="252" (
-    adb -s 0000000123456789 root
-    adb -s 0000000123456789 remount
+    adb -s 0000000123456789 root & adb -s 0000000123456789 remount
     adb -s 0000000123456789 shell tinyplay /data/48k_16bit_2ch.wav -D 0 -d 2
 )
