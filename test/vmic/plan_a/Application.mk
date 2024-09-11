42b2fd713b22fd1e8eaa0c90cb7d235bec860abe
Gateway11 <daixiang789@icloud.com>
2024-09-11 20:09:06 +0800

    update

--- test/vmic/plan_a/Application.mk
+++
@@ -1,6 +0,0 @@
-APP_ABI                 := arm64-v8a
-APP_CFLAGS              := -O3
-APP_CPPFLAGS            := -std=c++17 -frtti -Wno-register #-fpermissive
-APP_LDFLAGS             := -llog
-APP_PLATFORM            := android-32
-APP_STL                 := c++_static
