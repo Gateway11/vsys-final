ec1fd0c57dd2e00a13c0bc68d94e883fe45792da
Gateway11 <daixiang789@icloud.com>
2024-09-13 23:44:48 +0800

    update

--- test/virtual_mic/SharedMemoryHidl.cpp
+++ test/virtual_mic/SharedMemoryHidl.cpp
@@ -98,3 +98,77 @@ int main() {
 
     return 0;
 }
+// ####################################################################################
+http://aospxref.com/android-14.0.0_r2/xref/frameworks/av/media/libaudiohal/impl/EffectBufferHalHidl.cpp
+status_t EffectBufferHalHidl::init() {
+    sp<IAllocator> ashmem = IAllocator::getService("ashmem");
+    if (ashmem == 0) {
+        ALOGE("Failed to retrieve ashmem allocator service");
+        return NO_INIT;
+    }
+    status_t retval = NO_MEMORY;
+    Return<void> result = ashmem->allocate(
+            mBufferSize,
+            [&](bool success, const hidl_memory& memory) {
+                if (success) {
+                    mHidlBuffer.data = memory;
+                    retval = OK;
+                }
+            });
+    if (result.isOk() && retval == OK) {
+        mMemory = hardware::mapMemory(mHidlBuffer.data);
+        if (mMemory != 0) {
+            mMemory->update();
+            mAudioBuffer.raw = static_cast<void*>(mMemory->getPointer());
+            memset(mAudioBuffer.raw, 0, mMemory->getSize());
+            mMemory->commit();
+        } else {
+            ALOGE("Failed to map allocated ashmem");
+            retval = NO_MEMORY;
+        }
+    } else {
+        ALOGE("Failed to allocate %d bytes from ashmem", (int)mBufferSize);
+    }
+    return result.isOk() ? retval : FAILED_TRANSACTION;
+}
+// ####################################################################################
+
+//1
+#include <android/hidl/allocator/1.0/IAllocator.h>
+#include <hidlmemory/HidlMemoryDealer.h>
+#include <android/hidl/memory/block/1.0/types.h>
+#include <hidlmemory/mapping.h>
+
+using ::android::hidl::allocator::V1_0::IAllocator;
+using ::android::hardware::hidl_memory;
+using ::android::hardware::HidlMemoryDealer
+using ::android::hidl::memory::block::V1_0::MemoryBlock;
+
+sp<IAllocator> allocator = IAllocator::getService("ashmem");
+allocator->allocate(2048, [&](bool success, const hidl_memory& mem)
+{
+    if (!success) { /* error */ }
+    // you can now use the hidl_memory object 'mem' or pass it
+}));
+
+sp<HidlMemoryDealer> memory_dealer = HidlMemoryDealer::getInstance(mem);
+Return<void> Foo::getSome(getSome_cb _hidl_cb) {
+    MemoryBlock block = memory_dealer->allocate(1024);
+    if(HidlMemoryDealer::isOk(block)){
+        _hidl_cb(block);
+}
+
+Return<void> Foo::giveBack(const MemoryBlock& block) {
+    memory_dealer->deallocate(block.offset);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////
+
+#include <hidlmemory/mapping.h>
+#include <android/hidl/memory/1.0/IMemory.h>
+
+using ::android::hidl::memory::V1_0::IMemory;
+
+sp<RefBase> lockMemory(block.token);
+sp<IMemory> memory = mapMemory(block);
+uint8_t* data = static_cast<uint8_t*>(static_cast<void*>(memory->getPointer()));
