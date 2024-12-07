1b7be4ece24a6765808054d37564a87322a492ff
Gateway11 <daixiang789@icloud.com>
2024-12-07 15:29:28 +0800

    update

--- test/bsp/i2c/xml/busconfig.sh
+++ test/bsp/i2c/xml/busconfig.sh
@@ -4,7 +4,7 @@ i2c_dev=16
 xml_content=$(cat "adi_a2b_commandlist.xml")
 actions=$(echo "$xml_content" | grep -Eo '<action[^>]*>.*?</action>|<action[^>]*\s*/>')
 
-debug() { eval echo "Running command $((++line_count)): $1" && eval $1; }
+debug() { eval echo "Running command $((++line_count)): $1"; eval $1; }
 
 echo "$actions" | while read -r action; do
     instr=$(echo "$action" | sed -n 's/.*instr="\([^"]*\)".*/\1/p')
