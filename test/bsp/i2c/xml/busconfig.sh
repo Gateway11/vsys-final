dac2a25b4030595288134306e4f4a0eb1f0f965a
Gateway11 <daixiang789@icloud.com>
2024-12-07 13:25:45 +0800

    update

--- test/bsp/i2c/xml/busconfig.sh
+++ test/bsp/i2c/xml/busconfig.sh
@@ -4,6 +4,11 @@ line_count=0
 xml_content=$(cat "adi_a2b_commandlist.xml")
 actions=$(echo "$xml_content" | grep -Eo '<action[^>]*>.*?</action>|<action[^>]*\s*/>')
 
+function debug {
+    eval echo "Running command $((++line_count)): $1"
+    eval $1
+}
+
 echo "$actions" | while read -r action; do
     instr=$(echo "$action" | sed -n 's/.*instr="\([^"]*\)".*/\1/p')
     
@@ -23,16 +28,14 @@ echo "$actions" | while read -r action; do
     hex_i2caddr=$(echo $(printf "%02X" "$i2caddr") | sed 's/\([^ ]*\)/0x\1/g')
 
     if [[ "$instr" == "writeXbytes" ]]; then
-        set -x; i2cset -y 16 "$hex_i2caddr" "$hex_addr" $content_with_prefix; set +x
+        debug 'i2cset -y 16 "$hex_i2caddr" "$hex_addr" $content_with_prefix'
     elif [[ "$instr" == "read" ]]; then
-        set -x; i2cget -y 16 "$hex_i2caddr" "$hex_addr" "$((len - 1))"; set +x
+        debug 'i2cget -y 16 "$hex_i2caddr" "$hex_addr" "$((len - 1))"'
     elif [[ "$instr" == "delay" ]]; then
         delay_sec=$(bc <<< "scale=3; $((16#$content)) / 1000")
-        #set -x; sleep "$delay_sec"; set +x
-        set -x; perl -e "select(undef, undef, undef, $delay_sec)"; set +x
+        #debug 'sleep "$delay_sec"'
+        debug 'perl -e "select(undef, undef, undef, $delay_sec)"'
     else
         echo "Unknown instruction: $instr"
     fi
-
-    echo "------------- $((++line_count)) -------------"
 done
