3037bdc1c3dfe2c21c797f7082b7c032fe7c3c28
Gateway11 <daixiang789@icloud.com>
2024-12-10 21:28:03 +0800

    update

--- test/bsp/i2c/xml/a2b-pal-interface.h
+++ test/bsp/i2c/xml/a2b-pal-interface.h
@@ -18,7 +18,7 @@ and its licensors.
 #define I2C_DEV_PATH                    "/dev/i2c-16"
 #define A2B_MASTER_ADDR                 0x68
 #define A2B_SLAVE_ADDR                  0x69
-#define DSP_XXXX                        0x00
+#define DSP_XXXX                        0x38
 
 #define I2C_TIMEOUT_DEFAULT             700             /*!< timeout: 700ms*/
 #define I2C_RETRY_DEFAULT               3               /*!< retry times: 3 */
