2641801966d0832219e69d1124171fc2c597b3fa
Gateway11 <daixiang789@icloud.com>
2024-12-18 20:31:21 +0800

    update

--- test/bsp/i2c/xml/a2b-pal-interface-driver.c
+++ test/bsp/i2c/xml/a2b-pal-interface-driver.c
@@ -126,9 +126,7 @@ char* a2b_pal_File_Read(const char* filename, size_t* outSize) {
     return buffer;
 }
 
-void adi_a2b_Delay(uint32_t time) {
-    pr_info("Sleep 0x%02xms\n", time);
-
-    uint32_t time_us = time * 1000;
-    usleep_range(time_us, time_us + 1);
+void adi_a2b_Delay(uint32_t time_ms) {
+    pr_info("sleep 0x%02xms\n", time_ms);
+    mdelay(time_ms);
 }
