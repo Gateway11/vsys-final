bb2e47cca47b146eacc8c464fb1284bd447e58b0
daixiang <daixiang5@lenovo.com>
2024-12-25 17:49:09 +0800

    update

--- test/bsp/i2c/xml/run.sh
+++ test/bsp/i2c/xml/run.sh
@@ -8,6 +8,9 @@ if [ -n "$1" ]; then
     aplay -D hw:0,0 /usr/share/sounds/alsa/Rear_Right.wav
 
     amixer cset name="ADMAIF5 Mux" I2S7
+    #amixer cset name="MVC1 Mux" I2S7
+    #amixer cset name="MVC1 Volume" 1000
+    #amixer cset name="ADMAIF3 Mux" MVC1
 else
     arecord -D hw:0,4 -f S32_LE -c 8 -r 48000 -d 2 record.wav
     tar -cvf - record.wav | xz -9 --extreme | base64
