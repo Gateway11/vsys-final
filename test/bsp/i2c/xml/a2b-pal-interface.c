d0634cff34948bcaff3a4f8ad7f572ef4357dfb4
daixiang <daixiang5@lenovo.com>
2025-02-05 16:18:25 +0800

    update

--- test/bsp/i2c/xml/a2b-pal-interface.c
+++ test/bsp/i2c/xml/a2b-pal-interface.c
@@ -30,12 +30,14 @@ int32_t adi_a2b_I2C_Open(uint16_t deviceAddr) {
         return -1;
     }
 
+#if 0
     /* Set the I2C slave device address */
     if (ioctl(handle, I2C_SLAVE, deviceAddr) < 0) {
         perror("Can't set I2C slave address");
         close(handle); // Close the file descriptor before returning
         return -1;
     }
+#endif
 
     /* Set timeout and retry count */
     ioctl(handle, I2C_TIMEOUT, I2C_TIMEOUT_DEFAULT); // Set timeout
