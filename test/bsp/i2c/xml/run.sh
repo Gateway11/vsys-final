15ec2254c9d8a759c6686955cd4c2b9dcb13535e
Gateway11 <daixiang789@icloud.com>
2025-06-26 20:30:54 +0800

    update

--- test/bsp/i2c/xml/run.sh
+++ test/bsp/i2c/xml/run.sh
@@ -29,6 +29,7 @@ if [ -n "$1" ]; then
 else
     arecord -D hw:0,4 -f S32_LE -c 8 -r 48000 -d 1 record.wav
     #scp -J zhangcy52@192.168.1.100,xuwy@10.106.224.114 record.wav <>@10.106.250.32:/drives/c/Users/<>/record.wav
+    #ssh xuwy@10.106.224.114 "echo '123456' | sudo -S bash /home/xuwy/run.sh $1"
     tar -cf - record.wav | xz -9 --extreme | base64 -w 0 | split -b $((800 * 1024)) - part_
     #minicom -D /dev/ttyUSB2 -C /tmp/minicom.log
     #script -c "minicom -D /dev/ttyUSB2" /tmp/minicom.log
