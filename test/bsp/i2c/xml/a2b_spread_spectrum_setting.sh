f26768aa8342e3b756b50134d4da5c133056f065
Gateway11 <daixiang789@icloud.com>
2025-08-02 10:47:19 +0800

    update

--- test/bsp/i2c/xml/a2b_spread_spectrum_setting.sh
+++ test/bsp/i2c/xml/a2b_spread_spectrum_setting.sh
@@ -23,3 +23,19 @@ i2ctransfer -f -y 16 w2@0x68 0x01 0x20
 i2ctransfer -f -y 16 w2@0x69 0x40 $1
 i2ctransfer -f -y 16 w2@0x69 0x2E $2
 i2ctransfer -f -y 16 w2@0x69 0x30 $3
+
+#####################################################
+
+#Enable Voltage Monitor here
+i2ctransfer -f -y 16 w2@0x68 0xE0 0x01 #MMRPAGE
+i2ctransfer -f -y 16 w2@0x68 0x00 0x02 #VMTR_VEN, Enable VBUS
+i2ctransfer -f -y 16 w2@0x68 0x25 0x02 #VMTR_VMIN1, program Min voltage as 0
+i2ctransfer -f -y 16 w2@0x68 0x24 0x50 #VMTR_VMAX1, program Max voltage for 10V
+i2ctransfer -f -y 16 w2@0x68 0xE0 0x00 #MMRPAGE
+
+#Read Voltage Monitor
+i2ctransfer -f -y 16 w2@0x68 0xE0 0x01 #MMRPAGE
+i2ctransfer -f -y 16 w1@0x68 0x02 r1 #VMTR_MXSTAT, maxThres
+i2ctransfer -f -y 16 w1@0x68 0x03 r1 #VMTR_MNSTAT, minThres
+i2ctransfer -f -y 16 w1@0x68 0x23 r1 #VMTR_VLTG1, Read the measured voltage
+i2ctransfer -f -y 16 w2@0x68 0xE0 0x00 #MMRPAGE
