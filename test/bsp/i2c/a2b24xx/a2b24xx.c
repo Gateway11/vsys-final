7686bccbfaea209ce1bfafc812fb6a32ef86b418
daixiang <daixiang5@lenovo.com>
2025-12-02 14:48:11 +0800

    update

--- test/bsp/i2c/a2b24xx/a2b24xx.c
+++ test/bsp/i2c/a2b24xx/a2b24xx.c
@@ -577,7 +577,7 @@ static bool processSingleNode(struct a2b24xx *a2b24xx, uint8_t inode) {
     //    and clear interrupt pending bits (INTPEND=0xFF) and wait for 100ms
     adi_a2b_I2CWrite(dev, A2B_BASE_ADDR, 2, (uint8_t[]){A2B_REG_NODEADR, inode - 1});
     adi_a2b_I2CWrite(dev, A2B_BUS_ADDR, 2, (uint8_t[]){A2B_REG_SWCTL, 0x00});
-    adi_a2b_I2CWrite(dev, A2B_BUS_ADDR, 2, (uint8_t[]){A2B_REG_INTPND0, 0xFF});
+    adi_a2b_I2CWrite(dev, A2B_BUS_ADDR, 3, (uint8_t[]){A2B_REG_INTPND0, 0xFF, 0xFF});
     mdelay(100);
 
     // Now, periodically try to rediscover the slave-1 partially
