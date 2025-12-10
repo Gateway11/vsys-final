9695fb5c658a8feb5ab9c77a8f1981c41882865e
Gateway11 <daixiang789@icloud.com>
2025-12-10 23:15:51 +0800

    update

--- test/bsp/i2c/a2b24xx/a2b24xx.c
+++ test/bsp/i2c/a2b24xx/a2b24xx.c
@@ -857,17 +857,22 @@ static ssize_t a2b24xx_ctrl_write(struct file *file,
     } else if (strcmp(a2b24xx->command_buf, "Disable Fault Check") == 0) {
         a2b24xx_disable_fault_check(a2b24xx);
     // https://ez.analog.com/a2b/f/q-a/541883/ad2428-loopback-test
-    // https://juejin.cn/post/7197790401494925369
     } else if (sscanf(a2b24xx->command_buf, "Loopback Slave%hd %hd", &params[0], &params[1]) >= 1) {
         a2b24xx_disable_fault_check(a2b24xx);
 
         if (params[0] <= a2b24xx->num_nodes && (uint16_t)params[0] < sizeof(i2stest)) {
             if (params[0] < 0) {
+                // Master -> Host: Loopback Slave-1 1
+                // Host -> Master: Loopback Slave-1 0
                 adi_a2b_I2CWrite(a2b24xx->dev, A2B_BASE_ADDR, 2, (uint8_t[]){A2B_REG_DATCTL, 0x00});
                 adi_a2b_I2CWrite(a2b24xx->dev, A2B_BASE_ADDR, 2, (uint8_t[]){A2B_REG_I2STEST, i2stest[params[1]]});
             } else {
+                // Peripheral -> Slave: Loopback Slave0 0
+                // Slave -> DAC/Speaker: Loopback Slave0 1
                 mutex_lock(&a2b24xx->bus_lock);
                 adi_a2b_I2CWrite(a2b24xx->dev, A2B_BASE_ADDR, 2, (uint8_t[]){A2B_REG_NODEADR, params[0]});
+                adi_a2b_I2CWrite(a2b24xx->dev, A2B_BUS_ADDR, 2, (uint8_t[]){A2B_REG_I2SCFG, 0x11});
+                adi_a2b_I2CWrite(a2b24xx->dev, A2B_BUS_ADDR, 2, (uint8_t[]){A2B_REG_PDMCTL, 0x00});
                 adi_a2b_I2CWrite(a2b24xx->dev, A2B_BUS_ADDR, 2, (uint8_t[]){A2B_REG_I2STEST, i2stest[params[1]]});
                 mutex_unlock(&a2b24xx->bus_lock); // Release lock
             }
