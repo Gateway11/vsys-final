e3dc527e7ce10c573bb08fba25b71f29f889eeea
Gateway11 <daixiang789@icloud.com>
2025-12-21 15:28:53 +0800

    update

--- test/bsp/i2c/a2b24xx/a2b24xx.c
+++ test/bsp/i2c/a2b24xx/a2b24xx.c
@@ -942,11 +942,13 @@ static void a2b24xx_setup_work(struct work_struct *work)
     adi_a2b_NetworkSetup(a2b24xx->dev);
 
     for (int32_t i = (a2b24xx->num_actions - 1); i > 0; i--) {
-        if (a2b24xx->pA2BConfig[i].nAddr == A2B_REG_SLOTFMT) {
+        if (a2b24xx->pA2BConfig[i].nAddr == A2B_REG_SLOTFMT
+                && bus->pA2BConfig[i].nDeviceAddr == A2B_BASE_ADDR) {
             a2b24xx->master_fmt = a2b24xx->pA2BConfig[i].paConfigData[0];
             break;
         }
-        if (a2b24xx->pA2BConfig[i].nAddr == A2B_REG_NODEADR) {
+        if (a2b24xx->pA2BConfig[i].nAddr == A2B_REG_NODEADR
+                && bus->pA2BConfig[i].nDeviceAddr == A2B_BASE_ADDR) {
             a2b24xx->num_nodes = a2b24xx->pA2BConfig[i].paConfigData[0];
         }
     }
--- test/bsp/i2c/a2b24xx/multi_bus.c
+++ test/bsp/i2c/a2b24xx/multi_bus.c
@@ -927,11 +927,11 @@ static void a2b24xx_setup(struct a2b24xx *a2b24xx, struct a2b_bus *bus, uint8_t 
     adi_a2b_NetworkSetup(a2b24xx->dev, bus, parent);
 
     for (int32_t i = (bus->num_actions - 1); i >= 0; i--) {
-        if (bus->pA2BConfig[i].nAddr == A2B_REG_SLOTFMT) {
+        if (bus->pA2BConfig[i].nAddr == A2B_REG_SLOTFMT && bus->pA2BConfig[i].nDeviceAddr == A2B_BASE_ADDR) {
             bus->master_fmt = bus->pA2BConfig[i].paConfigData[0];
             break;
         }
-        if (bus->pA2BConfig[i].nAddr == A2B_REG_NODEADR) {
+        if (bus->pA2BConfig[i].nAddr == A2B_REG_NODEADR && bus->pA2BConfig[i].nDeviceAddr == A2B_BASE_ADDR) {
             bus->num_nodes = bus->pA2BConfig[i].paConfigData[0];
         }
     }
