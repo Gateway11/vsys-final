d78a4ba7acb74266327552aad37e3f6bb9565393
Gateway11 <daixiang789@icloud.com>
2026-05-28 23:21:53 +0800

    update

---
+++ test/bsp/i2c/a2b24xx/multi_bus/a2b24xx-i2c.c
@@ -0,0 +1,112 @@
+/*
+ * A2B24xx driver
+ *
+ * Copyright 2019 Analog Devices Inc.
+ * ADI Automotive Software Team, Bangalore
+ *
+ * Licensed under the GPL-2.
+ */
+
+#include <linux/i2c.h>
+#include <linux/mod_devicetable.h>
+#include <linux/module.h>
+#include <linux/regmap.h>
+#include <sound/soc.h>
+#include "a2b24xx.h"
+
+static int a2b24xx_i2c_probe(struct i2c_client *client,
+	const struct i2c_device_id *id)
+{
+	struct regmap_config config;
+	config = a2b24xx_regmap_config;
+	config.val_bits = 8;
+	config.reg_bits = 8;
+	return a2b24xx_probe(&client->dev,
+		devm_regmap_init_i2c(client, &config),
+		id->driver_data, NULL);
+}
+
+static void a2b24xx_i2c_remove(struct i2c_client *client)
+{
+	snd_soc_unregister_component(&client->dev);
+	a2b24xx_remove(&client->dev);
+	//return 0;
+}
+
+static int __maybe_unused a2b24xx_i2c_suspend(struct device *dev)
+{
+    return 0;
+}
+
+static int __maybe_unused a2b24xx_i2c_resume(struct device *dev)
+{
+    return 0;
+}
+
+static const struct dev_pm_ops a2b24xx_i2c_pm_ops = {
+    SET_SYSTEM_SLEEP_PM_OPS(a2b24xx_i2c_suspend, a2b24xx_i2c_resume)
+};
+
+#ifdef CONFIG_OF
+static const struct of_device_id a2b24xx_dt_ids[] = {
+	{ .compatible = "adi,a2b24xx", },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, a2b24xx_dt_ids);
+#endif
+
+static const struct i2c_device_id a2b24xx_i2c_ids[] = {
+	{ "a2b24xx", A2B24XX },
+	{ }
+};
+MODULE_DEVICE_TABLE(i2c, a2b24xx_i2c_ids);
+
+static struct i2c_driver a2b24xx_i2c_driver = {
+	.driver = {
+		.name = "a2b24xx",
+		.owner = THIS_MODULE,
+		.of_match_table = of_match_ptr(a2b24xx_dt_ids),
+		.pm = &a2b24xx_i2c_pm_ops,
+	},
+#if defined(NV_I2C_DRIVER_STRUCT_HAS_PROBE_NEW) /* Dropped on Linux 6.6 */
+	.probe_new	= a2b24xx_i2c_probe,
+#else
+	.probe		= a2b24xx_i2c_probe,
+#endif
+	.remove = a2b24xx_i2c_remove,
+	.id_table = a2b24xx_i2c_ids,
+};
+#if 0
+module_i2c_driver(a2b24xx_i2c_driver);
+#else
+
+/* ================= init / exit ================= */
+static int __init a2b24xx_init(void)
+{
+    int ret;
+
+    a2b24xx_class = class_create(THIS_MODULE, CLASS_NAME);
+    if (IS_ERR(a2b24xx_class))
+        return PTR_ERR(a2b24xx_class);
+
+    ret = i2c_add_driver(&a2b24xx_i2c_driver);
+    if (ret) {
+        class_destroy(a2b24xx_class);
+        return ret;
+    }
+    return 0;
+}
+
+static void __exit a2b24xx_exit(void)
+{
+    i2c_del_driver(&a2b24xx_i2c_driver);
+    class_destroy(a2b24xx_class);
+}
+
+module_init(a2b24xx_init);
+module_exit(a2b24xx_exit);
+#endif
+
+MODULE_DESCRIPTION("ASoC A2B24xx driver");
+MODULE_AUTHOR("ADI Automotive Software Team, Bangalore");
+MODULE_LICENSE("GPL");
