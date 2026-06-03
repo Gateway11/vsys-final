363074a2c123e8f1d714e99af43d7a3ee5709c09
Gateway11 <daixiang789@icloud.com>
2026-06-03 21:57:09 +0800

    update

--- test/bsp/switch/run.sh
+++ test/bsp/switch/run.sh
@@ -36,18 +36,18 @@ $CfgPrefix = "5192B-cpu1-swicth-cfg"
 & $SecureImageTool -imagefile="${BaseImage}" -partitionoffset=0x010000 -partitionsize=0x0F0000 -cfgimagefile="${CfgPrefix}.bin" -acceptanycfg
 mv ${BaseImage} ${DirName}\5192B-cpu1-${Version}.bin
 
-#$configs = @(
-#    @{ChipCpu = "5192A-cpu0"; CfgPrefix = "5192A-cpu0-swicth-cfg"; TargetId = 6},
-#    @{ChipCpu = "5192A-cpu1"; CfgPrefix = "5192A-cpu1-swicth-cfg"; TargetId = 7},
-#    @{ChipCpu = "5192B-cpu0"; CfgPrefix = "5192B-cpu0-swicth-cfg"; TargetId = 8},
-#    @{ChipCpu = "5192B-cpu1"; CfgPrefix = "5192B-cpu1-swicth-cfg"; TargetId = 9}
-#)
-#
-## 循环处理每个配置
-#foreach ($config in $configs) {
-#    cp ..\${BaseImage} .
-#    & $SwitchTool -switchcfg="$($config.CfgPrefix)-${CompanyName}.xml" -productioncfg="$($config.CfgPrefix).bin" -targetid=$($config.TargetId)
-#    & $SecureImageTool -imagefile="$BaseImage" -partitionoffset=0x100000 -partitionsize=0x0F0000 -cfgimagefile="$($config.CfgPrefix).bin" -acceptanycfg
-#    & $SecureImageTool -imagefile="$BaseImage" -partitionoffset=0x010000 -partitionsize=0x0F0000 -cfgimagefile="$($config.CfgPrefix).bin" -acceptanycfg
-#    mv ${BaseImage} "${DirName}\$($config.ChipCpu)-${Version}.bin"
-#}
+$configs = @(
+    @{ChipCpu = "5192A-cpu0"; CfgPrefix = "5192A-cpu0-swicth-cfg"; TargetId = 6},
+    @{ChipCpu = "5192A-cpu1"; CfgPrefix = "5192A-cpu1-swicth-cfg"; TargetId = 7},
+    @{ChipCpu = "5192B-cpu0"; CfgPrefix = "5192B-cpu0-swicth-cfg"; TargetId = 8},
+    @{ChipCpu = "5192B-cpu1"; CfgPrefix = "5192B-cpu1-swicth-cfg"; TargetId = 9}
+)
+
+# 循环处理每个配置
+foreach ($config in $configs) {
+    cp ..\${BaseImage} .
+    & $SwitchTool -switchcfg="$($config.CfgPrefix)-${CompanyName}.xml" -productioncfg="$($config.CfgPrefix).bin" -targetid=$($config.TargetId)
+    & $SecureImageTool -imagefile="$BaseImage" -partitionoffset=0x100000 -partitionsize=0x0F0000 -cfgimagefile="$($config.CfgPrefix).bin" -acceptanycfg
+    & $SecureImageTool -imagefile="$BaseImage" -partitionoffset=0x010000 -partitionsize=0x0F0000 -cfgimagefile="$($config.CfgPrefix).bin" -acceptanycfg
+    mv ${BaseImage} "${DirName}\$($config.ChipCpu)-${Version}.bin"
+}
