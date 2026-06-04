8c87a43f3c4f9e2e91da48802c8728ce04e66d37
Gateway11 <daixiang789@icloud.com>
2026-06-04 20:59:59 +0800

    update

--- test/bsp/switch/run.ps1
+++ test/bsp/switch/run.ps1
@@ -7,36 +7,6 @@ $BaseImage = "88Q5152_flash_7_5_base.bin"
 $DirName = "SwitchConfig_${CompanyName}_$(Get-Date -Format 'yyyyMMdd')_${Version}"
 New-Item -ItemType Directory -Path $DirName -Force
 
-cp ..\${BaseImage} .
-$CfgPrefix = "5192A-cpu0-swicth-cfg"
-& $SwitchTool -switchcfg="${CfgPrefix}-${CompanyName}.xml" -productioncfg="${CfgPrefix}.bin" -targetid=6
-& $SecureImageTool -imagefile="${BaseImage}" -partitionoffset=0x100000 -partitionsize=0x0F0000 -cfgimagefile="${CfgPrefix}.bin" -acceptanycfg
-& $SecureImageTool -imagefile="${BaseImage}" -partitionoffset=0x010000 -partitionsize=0x0F0000 -cfgimagefile="${CfgPrefix}.bin" -acceptanycfg
-mv ${BaseImage} ${DirName}\5192A-cpu0-${Version}.bin
-pause
-
-cp ..\${BaseImage} .
-$CfgPrefix = "5192A-cpu1-swicth-cfg"
-& $SwitchTool -switchcfg="${CfgPrefix}-${CompanyName}.xml" -productioncfg="${CfgPrefix}.bin" -targetid=7
-& $SecureImageTool -imagefile="${BaseImage}" -partitionoffset=0x100000 -partitionsize=0x0F0000 -cfgimagefile="${CfgPrefix}.bin" -acceptanycfg
-& $SecureImageTool -imagefile="${BaseImage}" -partitionoffset=0x010000 -partitionsize=0x0F0000 -cfgimagefile="${CfgPrefix}.bin" -acceptanycfg
-mv ${BaseImage} ${DirName}\5192A-cpu1-${Version}.bin
-
-cp ..\${BaseImage} .
-$CfgPrefix = "5192B-cpu0-swicth-cfg"
-& $SwitchTool -switchcfg="${CfgPrefix}-${CompanyName}.xml" -productioncfg="${CfgPrefix}.bin" -targetid=8
-& $SecureImageTool -imagefile="${BaseImage}" -partitionoffset=0x100000 -partitionsize=0x0F0000 -cfgimagefile="${CfgPrefix}.bin" -acceptanycfg
-& $SecureImageTool -imagefile="${BaseImage}" -partitionoffset=0x010000 -partitionsize=0x0F0000 -cfgimagefile="${CfgPrefix}.bin" -acceptanycfg
-mv ${BaseImage} ${DirName}\5192B-cpu0-${Version}.bin
-
-cp ..\${BaseImage} .
-$CfgPrefix = "5192B-cpu1-swicth-cfg"
-& $SwitchTool -switchcfg="${CfgPrefix}-${CompanyName}.xml" -productioncfg="${CfgPrefix}.bin" -targetid=9
-& $SecureImageTool -imagefile="${BaseImage}" -partitionoffset=0x100000 -partitionsize=0x0F0000 -cfgimagefile="${CfgPrefix}.bin" -acceptanycfg
-& $SecureImageTool -imagefile="${BaseImage}" -partitionoffset=0x010000 -partitionsize=0x0F0000 -cfgimagefile="${CfgPrefix}.bin" -acceptanycfg
-mv ${BaseImage} ${DirName}\5192B-cpu1-${Version}.bin
-
-
 $configs = @(
     @{ChipCpu = "5192A-cpu0"; CfgPrefix = "5192A-cpu0-switch-cfg"; TargetId = 6},
     @{ChipCpu = "5192A-cpu1"; CfgPrefix = "5192A-cpu1-switch-cfg"; TargetId = 7},
