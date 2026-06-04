332cb7953eb3c56596f9665ba99f027bbd019b88
daixiang <daixiang5@lenovo.com>
2026-06-04 13:33:45 +0800

    updsate

--- test/bsp/switch/run.ps1
+++ test/bsp/switch/run.ps1
@@ -38,15 +38,15 @@ mv ${BaseImage} ${DirName}\5192B-cpu1-${Version}.bin
 
 
 $configs = @(
-    @{ChipCpu = "5192A-cpu0"; CfgPrefix = "5192A-cpu0-swicth-cfg"; TargetId = "6"},
-    @{ChipCpu = "5192A-cpu1"; CfgPrefix = "5192A-cpu1-swicth-cfg"; TargetId = "7"},
-    @{ChipCpu = "5192B-cpu0"; CfgPrefix = "5192B-cpu0-swicth-cfg"; TargetId = "8"},
-    @{ChipCpu = "5192B-cpu1"; CfgPrefix = "5192B-cpu1-swicth-cfg"; TargetId = "9"}
+    @{ChipCpu = "5192A-cpu0"; CfgPrefix = "5192A-cpu0-switch-cfg"; TargetId = 6},
+    @{ChipCpu = "5192A-cpu1"; CfgPrefix = "5192A-cpu1-switch-cfg"; TargetId = 7},
+    @{ChipCpu = "5192B-cpu0"; CfgPrefix = "5192B-cpu0-switch-cfg"; TargetId = 8},
+    @{ChipCpu = "5192B-cpu1"; CfgPrefix = "5192B-cpu1-switch-cfg"; TargetId = 9}
 )
 
 foreach ($config in $configs) {
     cp ..\${BaseImage} .
-    & $SwitchTool -switchcfg="..\${CompanyName}\$($config.CfgPrefix)-${CompanyName}.xml" -productioncfg="$($config.CfgPrefix).bin" -targetid=$($config.TargetId)
+    & $SwitchTool -switchcfg="..\eabot\$($config.CfgPrefix)-${CompanyName}.xml" -productioncfg="$($config.CfgPrefix).bin" -targetid="$($config.TargetId)"
     & $SecureImageTool -imagefile="$BaseImage" -partitionoffset=0x100000 -partitionsize=0x0F0000 -cfgimagefile="$($config.CfgPrefix).bin" -acceptanycfg
     & $SecureImageTool -imagefile="$BaseImage" -partitionoffset=0x010000 -partitionsize=0x0F0000 -cfgimagefile="$($config.CfgPrefix).bin" -acceptanycfg
     mv ${BaseImage} "${DirName}\$($config.ChipCpu)-${Version}.bin" -Force
