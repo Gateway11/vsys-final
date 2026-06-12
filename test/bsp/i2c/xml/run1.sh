1948a3f9c438af08b020dcf6aefb0a258a012cd2
daixiang <daixiang5@lenovo.com>
2026-06-12 16:04:46 +0800

    update

--- test/bsp/i2c/xml/run1.sh
+++ test/bsp/i2c/xml/run1.sh
@@ -7,6 +7,7 @@ i2ctransfer -f -y 2 w2@0x68 0x53 0x06
 amixer cset name="I2S1 Mux" ADMAIF1
 amixer cset name="ADMAIF5 Mux" I2S1
 
+pkill -9 aplay
 aplay -D hw:0,0 out_sine_48000_16bit_32ch_10s.wav &
 sleep 1
 arecord -D hw:0,4 -f S16_LE -c 32 -r 48000 -d 5 record.wav
