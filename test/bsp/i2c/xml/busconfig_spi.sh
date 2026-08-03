6fdaf2557a37e17d2dabfe728d6049bb792921c5
daixiang <daixiang5@lenovo.com>
2026-08-03 17:49:52 +0800

    update

--- test/bsp/i2c/xml/busconfig_spi.sh
+++ test/bsp/i2c/xml/busconfig_spi.sh
@@ -32,6 +32,7 @@ echo "$actions" | while read -r action; do
                 ;;
             04)
                 sleep 0.002
+                #debug spidev_test -D "$spi_dev" -s 1000000 -b 8 -v -p "$spi_cmd_bytes\\x$(printf '%02X' "$((len-1-addr_width))")$dummy"
                 ;;
             05)
                 # Slave register read request
