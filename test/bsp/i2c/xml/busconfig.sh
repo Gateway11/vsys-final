03c81fbd2851e34e851f030e5487206abcbf6cfb
Gateway11 <daixiang789@icloud.com>
2026-08-04 21:07:41 +0800

    Revert "update"
    This reverts commit 43f1317bbc0f71c0bce9c52d3d1a3f3140208b75.

--- test/bsp/i2c/xml/busconfig.sh
+++ test/bsp/i2c/xml/busconfig.sh
@@ -14,9 +14,11 @@ grep '<action' "${1:-adi_a2b_commandlist.xml}" | while read -r action; do
         i2caddr=$(printf "0x%02X" "$(echo "${action#*i2caddr=\"}" | cut -d'"' -f1)")
 
         addr_bytes=""; for ((i = 0; i < $addr_width; i++)); do addr_bytes+=" 0x${addr:$((i * 2)):2}"; done
-        [[ "$instr" == "read" ]] && \
-            debug 'i2ctransfer -f -y $i2c_dev w$addr_width@"$i2caddr"$addr_bytes r"$((len - addr_width))"' || \
+        if [[ "$instr" == "read" ]]; then
+            debug 'i2ctransfer -f -y $i2c_dev w$addr_width@"$i2caddr"$addr_bytes r"$((len - addr_width))"'
+        else
             debug 'i2ctransfer -f -y $i2c_dev w$len@"$i2caddr"$addr_bytes $(echo "$content" | sed "s/\([^ ]*\)/0x\1/g")'
+        fi
     else
         delay_value=0
         for byte in $content; do delay_value=$(( (delay_value << 8) | (16#$byte) )); done
