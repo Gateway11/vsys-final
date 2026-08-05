61557f294ba7e9fd7762f0dfaec2b427586f79c9
daixiang <daixiang5@lenovo.com>
2026-08-05 15:49:04 +0800

    update

--- test/bsp/i2c/xml/busconfig.sh
+++ test/bsp/i2c/xml/busconfig.sh
@@ -10,19 +10,18 @@ grep '<action' "${1:-adi_a2b_commandlist_spi.xml}" | while read -r action; do
 
     addr_width=$(echo "${action#*addr_width=\"}" | cut -d'"' -f1)
     addr=$(printf "%0$((addr_width * 2))X" "$(echo "${action#* addr=\"}" | cut -d'"' -f1)")
+    addr_bytes=""; for ((i = 0; i < $addr_width; i++)); do addr_bytes+="\x${addr:$((i * 2)):2}"; done
     len=$(echo "${action#*len=\"}" | cut -d'"' -f1)
 
     if [[ "${action#*Protocol=\"}" == I2C* ]]; then
         i2caddr=$(printf "0x%02X" "$(echo "${action#*i2caddr=\"}" | cut -d'"' -f1)")
-        addr_bytes=""; for ((i = 0; i < $addr_width; i++)); do addr_bytes+=" 0x${addr:$((i * 2)):2}"; done
         [[ "$instr" == "read" ]] && { \
-            debug i2ctransfer -f -y $dev w$addr_width@$i2caddr$addr_bytes r$((len - addr_width)) || true; } || \
-            debug i2ctransfer -f -y $dev w$len@$i2caddr$addr_bytes $(echo "$content" | sed "s/\([^ ]*\)/0x\1/g")
+            debug i2ctransfer -f -y $dev w$addr_width@$i2caddr${addr_bytes//\\x/ 0x} r$((len - addr_width)) || true; } || \
+            debug i2ctransfer -f -y $dev w$len@$i2caddr${addr_bytes//\\x/ 0x} $(echo "$content" | sed "s/\([^ ]*\)/0x\1/g")
     else
         spiCmdWidth=$(echo "${action#*SpiCmdWidth=\"}" | cut -d'"' -f1)
         spiCmd=$(printf "%0$((spiCmdWidth * 2))X" "$(echo "${action#*SpiCmd=\"}" | cut -d'"' -f1)")
         spi_cmd_bytes=""; for ((i = 0; i < $spiCmdWidth; i++)); do spi_cmd_bytes+="\x${spiCmd:$((i * 2)):2}"; done
-        addr_bytes=""; for ((i = 0; i < $addr_width; i++)); do addr_bytes+="\x${addr:$((i * 2)):2}"; done
         dummy=""; for ((i=0; i<len-addr_width; i++)); do dummy="${dummy}\\x00"; done
         case "$spiCmd" in  #2/7  MOSI  | 0x02 | NODE | ADDR | DATA[0] | ... | DATA[N-1] |
             00|02*|07|C*)  #0    MOSI  | 0x00 | ADDR | DATA[0] | ... | DATA[N-1] |          #C* MOSI  | 110b:LEN-1 | NODE | ADDR |
